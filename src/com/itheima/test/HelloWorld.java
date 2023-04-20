205c0cde11a7463638f4228a7707735a0939a0a0
Rainday <1693313120@qq.com>
2023-04-20 16:38:23 +0800

    1.04 commit

--- src/com/itheima/test/HelloWorld.java
+++ src/com/itheima/test/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git !");
     }
 }
