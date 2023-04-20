4bc97fb981a235b420785ade907d9e3da5a6eb36
Rainday <1693313120@qq.com>
2023-04-20 19:05:04 +0800

    1.01 commit 功能-开发

--- src/com/itheima/test/HelloWorld.java
+++ src/com/itheima/test/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git !");
+        System.out.println("hello git4 !");
     }
 }
